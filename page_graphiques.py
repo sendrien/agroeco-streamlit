87bac97ff8adc7b16635c93f5e3bf60421ecae43
SANOU <sendrien@yahoo.com>
2025-07-12 22:40:21 +0200

    dfd

---
+++ page_graphiques.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_page_graphiques():
+    st.markdown("<h2 style='color:#5C7373;'>Graphiques</h2>", unsafe_allow_html=True)
+    st.write("Les visualisations graphiques seront bientôt disponibles ici.")
