ece3216d028a1ed97a2fb2398ac47823540ef630
SANOU <sendrien@yahoo.com>
2025-07-12 20:58:26 +0200

    dhd

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ h2 {
     transition: color 0.4s;
 }
 .stTabs [data-baseweb="tab-list"] {
-    border-bottom: 2.5px solid #027368 !important;
+    border-bottom: 50.5px solid #027368 !important;
 }
 .stTabs [data-baseweb="tab"] {
     color: #5C7373 !important;
@@ -147,9 +147,9 @@ h2 {
     transition: color 0.4s;
 }
 .stTabs [aria-selected="true"] {
-    color: white !important;
-    border-bottom: 12px solid #027368 !important;
-    background: #027368 !important;
+    color: #011F26 !important;
+    border-bottom: 3px solid #027368 !important;
+    background: #02736810 !important;
     transition: color 0.4s, border 0.4s;
 }
 
