9dab8066df809803a146e6725f322fff24fbf420
SANOU <sendrien@yahoo.com>
2025-07-13 12:44:45 +0200

    zf

--- page_graphiques.py
+++ page_graphiques.py
@@ -119,7 +119,7 @@ def bar_chart_anim(radar_df, dim_labels, cat_labels, cat_idx=0):
                 "type": "buttons",
                 "showactive": False,
                 "y": 1.16,
-                "x": 0.5,
+                "x": 0.9,
                 "xanchor": "left",
                 "yanchor": "top",
                 "buttons": [{
