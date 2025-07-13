220c7f55a52eee57e307eb91b32535a186565537
SANOU <sendrien@yahoo.com>
2025-07-13 10:42:13 +0200

    tgh

--- page_graphiques.py
+++ page_graphiques.py
@@ -38,26 +38,33 @@ def show_page_graphiques():
     angles = np.linspace(0, 2 * np.pi, N, endpoint=False).tolist()
     angles += angles[:1]
 
-    fig, ax = plt.subplots(figsize=(2.8, 2.8), subplot_kw=dict(polar=True))  # Taille réduite
+    # Figure plus compacte
+    fig, ax = plt.subplots(figsize=(2.2, 2.2), subplot_kw=dict(polar=True))
 
     colors = plt.cm.tab10.colors
     for idx, (cat, color) in enumerate(zip(categories_labels, colors)):
         values = radar_df.loc[cat].tolist()
         values += values[:1]
-        ax.plot(angles, values, label=cat, color=color, linewidth=1.3)
+        ax.plot(angles, values, label=cat, color=color, linewidth=1.1)
 
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels(labels, fontsize=8)
     ax.set_yticks([1, 1.5, 2, 2.5, 3])
     ax.set_yticklabels([str(x) for x in [1, 1.5, 2, 2.5, 3]], fontsize=7)
     ax.set_ylim(0, 3.5)
+    ax.spines['polar'].set_color('#000')
+    ax.spines['polar'].set_linewidth(1)
 
-    # Pas de titre sur le graphique
-    # Légende compacte en bas du graphique
-    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.20), fontsize=7, frameon=False, ncol=1)
+    # Légende compacte sous le graphique
+    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.18), fontsize=7, frameon=False, ncol=1)
 
-    st.pyplot(fig)
+    # Pas de titre
+    plt.tight_layout(pad=0.1)
+    
+    # Centrage horizontal dans Streamlit
+    col1, col2, col3 = st.columns([1,2,1])
+    with col2:
+        st.pyplot(fig)
 
-# Utilisation dans la page
 if __name__ == "__main__" or "streamlit" in __name__:
     show_page_graphiques()
