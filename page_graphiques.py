baf6a283d28ecbaf74687e12935bba85fc34c173
SANOU <sendrien@yahoo.com>
2025-07-13 12:56:49 +0200

    jhfg

--- page_graphiques.py
+++ page_graphiques.py
@@ -123,7 +123,7 @@ def bar_chart_anim(radar_df, dim_labels, cat_labels, cat_idx=0):
                 "type": "buttons",
                 "showactive": False,
                 "y": 1.25,
-                "x": -0.08,
+                "x": -0.084,
                 "xanchor": "left",
                 "yanchor": "top",
                 "buttons": [{
