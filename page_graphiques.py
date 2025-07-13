146a3b2a38d428c7cd8837a7290970e61dc50688
SANOU <sendrien@yahoo.com>
2025-07-13 21:43:43 +0200

    lljk

--- page_graphiques.py
+++ page_graphiques.py
@@ -221,7 +221,7 @@ def bar_effectifs_stacked_anim(eff_df):
             updatemenus=[{
                 "type": "buttons",
                 "showactive": False,
-                "y": 1.13, "x": -0.8,
+                "y": 1.13, "x": -0.6,
                 "xanchor": "left", "yanchor": "top",
                 "buttons": [{
                     "label": "Dessiner",
