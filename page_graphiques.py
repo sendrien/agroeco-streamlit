3daf1a825aec65e699e6c113e788a85a5d1890c5
SANOU <sendrien@yahoo.com>
2025-07-13 12:40:23 +0200

    zerghgd

--- page_graphiques.py
+++ page_graphiques.py
@@ -132,6 +132,8 @@ def bar_chart_anim(radar_df, dim_labels, cat_labels, cat_idx=0):
                 }],
                 "bgcolor": "#fff",
                 "bordercolor": "#027368",
+                "borderwidth": 1.8,
+                "font": {"color": "blue", "size": 15}
             }]
         )
     )
